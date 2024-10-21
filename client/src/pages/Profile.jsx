ae71ea7a62b4ef331f67de2cdf5287199543222a
javilian98 <javierlian98@gmail.com>
2024-10-21 17:53:34 +0800

    implemented profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([
+    {
+      owner: "0xabc",
+      title: "Fund Example 1",
+      description: "Description of this campaign",
+      target: 0.15,
+      deadline: 1729437427,
+      amountCollected: 0.1,
+      image:
+        "https://www.geekawhat.com/wp-content/uploads/2024/03/FI_6500X-White-4070-Ti-SUPER-PC-Build.jpg",
+    },
+  ]);
+
+  const { address, contract, getUserCampaigns } = useStateContext();
+
+  // const fetchCampaigns = () => {
+  //   setIsLoading(true)
+  //   const data = await getUserCampaigns()
+  //   setCampaigns(data)
+  //   setIsLoading(false)
+  // }
+
+  // useEffect(() => {
+  //   if (contract) fetchCampaigns()
+  // }, [address, contract])
+
+  return (
+    <DisplayCampaigns
+      title={"All Campaigns"}
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
 };
 
 export default Profile;
