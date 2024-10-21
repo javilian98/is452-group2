7d099a94717c9e86b4abf25a2543ae86241c841f
javilian98 <javierlian98@gmail.com>
2024-10-21 17:16:02 +0800

    Implemented Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+// import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([
+    {
+      owner: "0xabc",
+      title: "Fund Example 1",
+      description: "Description of this campaign",
+      target: 0.15,
+      deadline: 1729437427,
+      amountCollected: 0.1,
+      image:
+        "https://www.geekawhat.com/wp-content/uploads/2024/03/FI_6500X-White-4070-Ti-SUPER-PC-Build.jpg",
+    },
+  ]);
 
-export default Home
+  // const { address, contract, getCampaigns } = useStateContext()
+
+  // const fetchCampaigns = () => {
+  //   setIsLoading(true)
+  //   const data = await getCampaigns()
+  //   setCampaigns(data)
+  //   setIsLoading(false)
+  // }
+
+  // useEffect(() => {
+  //   if (contract) fetchCampaigns()
+  // }, [address, contract])
+
+  return (
+    <DisplayCampaigns
+      title={"All Campaigns"}
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
+};
+
+export default Home;
